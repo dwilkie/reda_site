5ee48b939a6723aafc2ff0494c8506a53026ea03
David Wilkie <dwilkie@gmail.com>
2011-07-18 13:03:28 +0700

    Refinery CMS REDA app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reda::Application.config.secret_token = 'a868bf2c1f2f7546b61c7e56f6fdab4a60ba442bf8dac6ef8e99bd30c31d8692125a7427463e3b6807bc407be6c24cd52de07dec740521f64c5fd92491028586'
